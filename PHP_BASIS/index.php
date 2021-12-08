16f4b93a9c0aa16001e2d188cbc9e9ca3f126316
tjidde-nl <tjidde-nl@users.noreply.github.com>
2021-12-08 11:57:21 +0100

    Made more simple

--- PHP_BASIS/index.php
+++ PHP_BASIS/index.php
@@ -1,8 +1,5 @@
-<?php include_once ("functions.php");
-if(!empty($_POST)){
-getContent("contacted",$_POST);}
-else{
-    if(empty(($_GET)))getContent("home");
-    else getContent($_GET["page"]);
-}
-?>
+<?php
+include_once("functions.php");
+
+if (empty(($_GET))) getContent("home"); else getContent($_GET["page"]);
+
