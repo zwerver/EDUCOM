cd5d562a431a38632c15c65ae9e79a977acc6f10
Geert Weggemans <geert@man-kind.nl>
2021-12-13 08:29:48 +0100

    Update index.php
    Comments //GW added

--- PHP_BASIS/index.php
+++ PHP_BASIS/index.php
@@ -1,10 +1,12 @@
 <?php
 include_once("functions.php");
+//GW : kijk naar je naamgeving, doet deze functie enkel wat zijn naam zegt?
 getRequestedPage();
 
 $key = "page";
 function getRequestedPage()
 {
+// GW: Zowel in GET als POST rekening houden met ontbrekende parameter!    
     $requested_type = $_SERVER['REQUEST_METHOD'];
     if ($requested_type == 'POST')
     {
